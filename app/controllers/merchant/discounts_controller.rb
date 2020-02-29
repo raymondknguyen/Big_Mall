798673d94e9b93de838e3aa27dbfb62f81192003
Raymond Nguyen <rayknguyen@gmail.com>
2020-02-29 15:01:58 -0700

    Add method for creating discounts

--- app/controllers/merchant/discounts_controller.rb
+++ app/controllers/merchant/discounts_controller.rb
@@ -1,4 +1,16 @@
 class Merchant::DiscountsController < ApplicationController
   def new
+    @merchant = Merchant.find(params[:merchant_id])
+  end
+
+  def create
+    merchant = Merchant.find(params[:merchant_id])
+    @discount = merchant.discounts.create(discount_params)
+  end
+
+  private
+
+  def discount_params
+    params.permit(:name, :percentage, :min_items, :description)
   end
 end
