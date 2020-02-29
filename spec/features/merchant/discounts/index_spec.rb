66f2c5a9759e4793a470bf453eff72a9a0165093
Raymond Nguyen <rayknguyen@gmail.com>
2020-02-29 15:40:01 -0700

    Add edge case to index

--- spec/features/merchant/discounts/index_spec.rb
+++ spec/features/merchant/discounts/index_spec.rb
@@ -3,14 +3,18 @@ require "rails_helper"
 RSpec.describe "As a merchant employee" do
   before :each do
     @merchant = create(:random_merchant)
+    @merchant_2 = create(:random_merchant)
     @merchant_employee = create(:merchant_user, merchant: @merchant)
     @discount_1 = @merchant.discounts.create(name: "10% Discount!", percentage: 10, min_items: 10, description: "If you buy 10 items or more you get 10% off each items from this merchant")
     @discount_2 = @merchant.discounts.create(name: "20% Discount!", percentage: 20, min_items: 20, description: "If you buy 20 items or more you get 10% off each items from this merchant")
+    @discount_3 = @merchant_2.discounts.create(name: "crazy discount", percentage: 10, min_items: 10, description: "If you buy 20 items or more you get 10% off each items from this merchant")
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@merchant_employee)
   end
 
   it "can see all discount of this merchant" do
     visit "/merchant/discounts"
+
+    expect(page).to_not have_content(@discount_3.name)
     
     within "#discount-#{@discount_1.id}" do
       expect(page).to have_content(@discount_1.name)
