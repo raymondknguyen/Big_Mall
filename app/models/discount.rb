c393873102a7c031e808fbd73c5478df278a6e36
Raymond Nguyen <rayknguyen@gmail.com>
2020-03-02 16:30:34 -0700

    Add active record

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -4,4 +4,8 @@ class Discount <ApplicationRecord
                         :min_items, 
                         :description
   belongs_to :merchant
+
+  def items_with_discount
+    Discount.joins(merchant: :items).where('items.id = ?', cart.contents.keys)
+  end
 end
