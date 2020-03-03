8715c32f966d4d3804eaf72b89f8ffe325201460
Raymond Nguyen <rayknguyen@gmail.com>
2020-03-03 14:48:32 -0700

    Ad validations to discount

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -3,6 +3,8 @@ class Discount <ApplicationRecord
                         :percentage, 
                         :min_items, 
                         :description
+  validates_numericality_of :percentage, greater_than: 0, less_than_or_equal_to: 100
+  validates_numericality_of :min_items, greater_than: 0
   belongs_to :merchant
 
   def items_with_discount
